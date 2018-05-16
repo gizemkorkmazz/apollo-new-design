4916ff580518ad4db37a620de19a5d6ba9dcab3c
erkangozukucuk1 <erkan_gozukucuk@hotmail.com>
2018-05-16 14:56:56 +0300

    animations

--- apollo-new-design/src/components/KokpitView.js
+++ apollo-new-design/src/components/KokpitView.js
@@ -1,9 +1,54 @@
-import React from 'react';
-import { Text, Image, View } from 'react-native';
-export const KokpitView = ({ imageSource1, imageSource2, textTitle1, textTitle2 }) => {
-	return (
-		<View
-			style={{
+import React, { Component } from 'react';
+import {
+	Text,
+	Image,
+	View,
+	StyleSheet,
+	Button,
+	TouchableWithoutFeedback,
+	UIManager,
+	Platform,
+	LayoutAnimation
+} from 'react-native';
+
+const selectOpened = (opened, style) => (opened ? style.opened : style.closed);
+var CustomLayoutAnimation = {
+	duration: 200,
+	create: {
+		type: LayoutAnimation.Types.easeOut,
+		property: LayoutAnimation.Properties.scaleXY
+	},
+	update: {
+		type: LayoutAnimation.Types.linear
+	},
+	delete: {
+		duration: 50,
+		type: LayoutAnimation.Types.easeOut,
+		property: LayoutAnimation.Properties.scaleXY
+	}
+	// create: {
+	// 	type: LayoutAnimation.Types.linear,
+	// 	property: LayoutAnimation.Properties.opacity
+	// },
+	// update: {
+	// 	type: LayoutAnimation.Types.curveEaseInEaseOut
+	// }
+};
+const getStyles = opened =>
+	StyleSheet.create({
+		container: selectOpened(opened, {
+			opened: {
+				flexDirection: 'column',
+				justifyContent: 'flex-start',
+				backgroundColor: '#616788',
+				alignItems: 'center',
+				borderRadius: 20,
+				marginLeft: 20,
+				marginRight: 20,
+				marginBottom: 10,
+				height: 160
+			},
+			closed: {
 				flexDirection: 'row',
 				justifyContent: 'space-between',
 				backgroundColor: '#616788',
@@ -13,26 +58,74 @@ export const KokpitView = ({ imageSource1, imageSource2, textTitle1, textTitle2 
 				marginRight: 20,
 				marginBottom: 10,
 				height: 60
-			}}
-		>
-			<View style={{ flexDirection: 'row', alignItems: 'center' }}>
-				<Image source={imageSource1} style={{ width: 50, height: 50 }} />
-				<Text style={{ color: 'white', textAlign: 'left' }}> {textTitle1}</Text>
-			</View>
-			<View style={{ flexDirection: 'row', alignItems: 'center' }}>
-				<Text style={{ color: 'white', marginRight: 10 }}> {textTitle2}</Text>
+			}
+		}),
+		graph: selectOpened(opened, {
+			opened: {
+				width: 160,
+				height: 100,
+				right: 0,
+				borderRadius: 10
+			},
+			closed: {
+				width: 60,
+				height: 60,
+				right: 0,
+				borderTopRightRadius: 20,
+				borderBottomRightRadius: 20
+			}
+		}),
+		valueText: selectOpened(opened, {
+			opened: { color: 'white', width: 0, height: 0 },
+			closed: { color: 'white', marginRight: 10 }
+		})
+	});
 
-				<Image
-					source={imageSource2}
-					style={{
-						width: 60,
-						height: 60,
-						right: 0,
-						borderTopRightRadius: 20,
-						borderBottomRightRadius: 20
-					}}
-				/>
-			</View>
-		</View>
-	);
-};
+export default class KokpitView extends Component {
+	constructor(props) {
+		super(props);
+		if (Platform.OS === 'android') {
+			UIManager.setLayoutAnimationEnabledExperimental(true);
+		}
+		this.state = {
+			opened: false
+		};
+	}
+
+	renderValueText(textTitle2) {
+		if (this.state.opened) {
+			return <View />;
+		}
+		return <Text style={{ color: 'white', marginRight: 10 }}>{textTitle2}</Text>;
+	}
+	render() {
+		const { imageSource1, imageSource2, textTitle1, textTitle2 } = this.props;
+		const { opened } = this.state;
+		const styles = getStyles(opened);
+
+		return (
+			<TouchableWithoutFeedback
+				onPress={() => {
+					LayoutAnimation.configureNext(CustomLayoutAnimation);
+					this.setState({ opened: !this.state.opened });
+				}}
+			>
+				<View style={styles.container}>
+					<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+						<Image source={imageSource1} style={{ width: 50, height: 50 }} />
+						<Text
+							ellipsizeMode="tail"
+							style={{ maxWidth: this.state.opened ? null : 150, color: 'white', textAlign: 'left' }}
+						>
+							{textTitle1}
+						</Text>
+					</View>
+					<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+						{this.renderValueText(textTitle2)}
+						<Image source={imageSource2} style={styles.graph} />
+					</View>
+				</View>
+			</TouchableWithoutFeedback>
+		);
+	}
+}
