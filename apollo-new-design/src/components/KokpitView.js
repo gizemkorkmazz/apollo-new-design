6fb6e97cc0eb657ce93a48a9a2359c8338191775
erkangozukucuk1 <erkan_gozukucuk@hotmail.com>
2018-05-24 15:03:08 +0300

    Revert "animation"
    This reverts commit a4213445f8273c6a19c73478280046cead73c488.

--- apollo-new-design/src/components/KokpitView.js
+++ apollo-new-design/src/components/KokpitView.js
@@ -5,75 +5,127 @@ import {
 	View,
 	StyleSheet,
 	Button,
-	TouchableOpacity,
+	TouchableWithoutFeedback,
 	UIManager,
 	Platform,
-	LayoutAnimation,
-	TouchableHighlight
+	LayoutAnimation
 } from 'react-native';
 
+const selectOpened = (opened, style) => (opened ? style.opened : style.closed);
+var CustomLayoutAnimation = {
+	duration: 200,
+	create: {
+		type: LayoutAnimation.Types.easeOut,
+		property: LayoutAnimation.Properties.scaleXY
+	},
+	update: {
+		type: LayoutAnimation.Types.linear
+	},
+	delete: {
+		duration: 50,
+		type: LayoutAnimation.Types.easeOut,
+		property: LayoutAnimation.Properties.scaleXY
+	}
+	// create: {
+	// 	type: LayoutAnimation.Types.linear,
+	// 	property: LayoutAnimation.Properties.opacity
+	// },
+	// update: {
+	// 	type: LayoutAnimation.Types.curveEaseInEaseOut
+	// }
+};
+const getStyles = opened =>
+	StyleSheet.create({
+		container: selectOpened(opened, {
+			opened: {
+				flexDirection: 'column',
+				justifyContent: 'flex-start',
+				backgroundColor: '#616788',
+				alignItems: 'center',
+				borderRadius: 20,
+				marginLeft: 20,
+				marginRight: 20,
+				marginBottom: 10,
+				height: 160
+			},
+			closed: {
+				flexDirection: 'row',
+				justifyContent: 'space-between',
+				backgroundColor: '#616788',
+				alignItems: 'center',
+				borderRadius: 20,
+				marginLeft: 20,
+				marginRight: 20,
+				marginBottom: 10,
+				height: 60
+			}
+		}),
+		graph: selectOpened(opened, {
+			opened: {
+				width: 160,
+				height: 100,
+				right: 0,
+				borderRadius: 10
+			},
+			closed: {
+				width: 60,
+				height: 60,
+				right: 0,
+				borderTopRightRadius: 20,
+				borderBottomRightRadius: 20
+			}
+		}),
+		valueText: selectOpened(opened, {
+			opened: { color: 'white', width: 0, height: 0 },
+			closed: { color: 'white', marginRight: 10 }
+		})
+	});
+
 export default class KokpitView extends Component {
-	componentDidMount() {
-		setTimeout(this.measureProgressBar, 100);
-		this.yPos = 0;
+	constructor(props) {
+		super(props);
+		if (Platform.OS === 'android') {
+			UIManager.setLayoutAnimationEnabledExperimental(true);
+		}
+		this.state = {
+			opened: false
+		};
 	}
 
-	measureProgressBar = () => {
-		this.refs.touchable.measure(this.setWidthProgressMaxSize);
-	};
-
-	setWidthProgressMaxSize = (x, y, width, height, px, py) => {
-		this.yPos = py;
-		console.log(x, y, width, height, px, py);
-		// this.setState({ progressMaxSize: width });
-	};
-
+	renderValueText(textTitle2) {
+		if (this.state.opened) {
+			return <View />;
+		}
+		return <Text style={{ color: 'white', marginRight: 10 }}>{textTitle2}</Text>;
+	}
 	render() {
-		const { imageSource1, imageSource2, textTitle1, textTitle2, onPress } = this.props;
+		const { imageSource1, imageSource2, textTitle1, textTitle2 } = this.props;
+		const { opened } = this.state;
+		const styles = getStyles(opened);
 
 		return (
-			<TouchableOpacity
-				activeOpacity={1}
-				ref="touchable"
+			<TouchableWithoutFeedback
 				onPress={() => {
-					onPress(this.yPos);
-					// this.setState({ opened: !this.state.opened });
+					LayoutAnimation.configureNext(CustomLayoutAnimation);
+					this.setState({ opened: !this.state.opened });
 				}}
 			>
-				<View
-					style={{
-						flexDirection: 'row',
-						justifyContent: 'space-between',
-						backgroundColor: '#616788',
-						alignItems: 'center',
-						borderRadius: 20,
-						marginLeft: 20,
-						marginRight: 20,
-						marginBottom: 10,
-						height: 60
-					}}
-				>
+				<View style={styles.container}>
 					<View style={{ flexDirection: 'row', alignItems: 'center' }}>
 						<Image source={imageSource1} style={{ width: 50, height: 50 }} />
-						<Text ellipsizeMode="tail" style={{ maxWidth: 150, color: 'white', textAlign: 'left' }}>
+						<Text
+							ellipsizeMode="tail"
+							style={{ maxWidth: this.state.opened ? null : 150, color: 'white', textAlign: 'left' }}
+						>
 							{textTitle1}
 						</Text>
 					</View>
 					<View style={{ flexDirection: 'row', alignItems: 'center' }}>
-						<Text style={{ textAlign: 'right', color: 'white', marginRight: 10 }}>{textTitle2}</Text>
-						<Image
-							source={imageSource2}
-							style={{
-								width: 60,
-								height: 60,
-								right: 0,
-								borderTopRightRadius: 20,
-								borderBottomRightRadius: 20
-							}}
-						/>
+						{this.renderValueText(textTitle2)}
+						<Image source={imageSource2} style={styles.graph} />
 					</View>
 				</View>
-			</TouchableOpacity>
+			</TouchableWithoutFeedback>
 		);
 	}
 }
